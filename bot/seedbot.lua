064364f52b29a823062f60f7144d4e7059d38c00
radiioactive <mr.masyh1375@gmail.com>
2016-07-14 19:04:12 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,23 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {radiioactive_bot},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[radioactive v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
 https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Radioactive :)
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+@radiioactive
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: @gong1375
+Persian: @gong1375
 ]],
     help_text_realm = [[
 Realm Commands:
